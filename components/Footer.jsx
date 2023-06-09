b1eb2451efd175f63be15ac9887663db439c766b
Yash Chauhan <ychauhan2@horizon.csueastbay.edu>
2023-06-09 08:41:14 -0700

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,9 +2,9 @@ import { Box } from '@chakra-ui/react';
 
 const Footer = () => (
     <Box textAlign='center' p='5' color='gray.600'>
-        Made with ❤️ by Shriansh Agarwal
+        Made with ❤️ by Yash Chauhan
         <br />
-        &copy; 2022 &mdash; getApartments.in
+        &copy; 2022 &cyash; getApartments.in
     </Box>
 );
 
